e3831dba33577f869672c3de0ec850cedacf457d
onofresalvador <onofresalvador01@gmail.com>
2018-04-27 00:02:16 -0400

    Upgrade the detail xml, still need adjustments, create a new json object and  update the DetailActivity java class.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,10 +3,13 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
@@ -14,23 +17,38 @@ public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private ImageView sandwichImagen;
+      private TextView originTx;
+    private TextView originLabel;
+    private TextView descriptionTx;
+    private TextView ingredientTx;
+    private TextView alsoKnownTx;
+    private TextView alsoKnownLabel;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+
+        sandwichImagen = findViewById(R.id.image_iv);
+        originTx = findViewById(R.id.origin_tv);
+        originLabel = findViewById(R.id.placeOfOrigin_label);
+        descriptionTx = findViewById(R.id.description_tv);
+        ingredientTx = findViewById(R.id.ingredients_tv);
+        alsoKnownTx = findViewById(R.id.also_known_tv);
+        alsoKnownLabel = findViewById(R.id.alsoKnownAs_label);
 
         Intent intent = getIntent();
         if (intent == null) {
-            closeOnError();
+            dataError();
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
-            // EXTRA_POSITION not found in intent
-            closeOnError();
+            dataError();
             return;
         }
 
@@ -38,25 +56,58 @@ public class DetailActivity extends AppCompatActivity {
         String json = sandwiches[position];
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
-            // Sandwich data unavailable
-            closeOnError();
+             dataError();
             return;
         }
 
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
+        populateUI(sandwich);
         setTitle(sandwich.getMainName());
     }
 
-    private void closeOnError() {
+    private void dataError() {
         finish();
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+        if (sandwich.getAlsoKnownAs() != null && sandwich.getAlsoKnownAs().size() > 0) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(sandwich.getAlsoKnownAs().get(0));
+
+            for (int i = 1; i < sandwich.getAlsoKnownAs().size(); i++) {
+                stringBuilder.append(", ");
+                stringBuilder.append(sandwich.getAlsoKnownAs().get(i));
+            }
+            alsoKnownTx.setText(stringBuilder.toString());
+        } else {
+            alsoKnownTx.setVisibility(View.GONE);
+            alsoKnownLabel.setVisibility(View.GONE);
+        }
+
+
+        if (sandwich.getPlaceOfOrigin().isEmpty()) {
+            originTx.setVisibility(View.GONE);
+            originLabel.setVisibility(View.GONE);
+        } else {
+            originTx.setText(sandwich.getPlaceOfOrigin());
+        }
+
+           descriptionTx.setText(sandwich.getDescription());
+
+        if (sandwich.getIngredients() != null && sandwich.getIngredients().size() > 0) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(sandwich.getIngredients().get(0));
+
+            for (int i = 1; i < sandwich.getIngredients().size(); i++) {
+                stringBuilder.append("\n");
+                stringBuilder.append(sandwich.getIngredients().get(i));
+            }
+            ingredientTx.setText(stringBuilder.toString());
+        }
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(sandwichImagen);
 
     }
 }
